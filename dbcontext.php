3da5f262aa93e00f33ac59899847391aab8f67de
Sachisal33 <hellracer1990@gmail.com>
2021-11-30 11:18:21 +0100

    dbcon

--- dbcontext.php
+++ dbcontext.php
@@ -4,8 +4,8 @@ function OpenCon()
  {
  $dbhost = "localhost";
  $dbuser = "root";
- $dbpass = "1234";
- $db = "example";
+ $dbpass = "";
+ $db = "dmshop";
 
 
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
--- index.php
+++ index.php
@@ -2,5 +2,17 @@
 include 'dbcontext.php';
 $conn = OpenCon();
 echo "Connected Successfully";
-CloseCon($conn);
+
+$sql = "SELECT * FROM produkte";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo "id: " . $row["ID"]. " - Name: " . $row["Name"]. " - Preis: " . $row["Preis"]. " - Kategorie: " . $row["Kategorie"]. " - Beschreibung: " . $row["Beschreibung"]. " - ImageSource: " . $row["ImageSource"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  }
+
 ?>
